7ca3dffe5277ac95d4093bcedaba027f517eb587
Jhonny Robert (Zigu) <jhonnyrobert.s@outlook.com>
2022-06-09 15:58:48 -0300

    added HTML report

---
+++ Test/script.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
+
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
